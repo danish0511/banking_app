3c59df1392e5e9e57784f2d9e9d953b35b558c36
danish0511 <dsdd0511@gmail.com>
2024-08-03 16:00:58 +0530

    left sidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,15 +1,33 @@
+import MobileNav from "@/components/MobileNav";
+import Sidebar from "@/components/Sidebar";
 import type { Metadata } from "next";
 import { Inter, IBM_Plex_Serif } from "next/font/google";
+import Image from "next/image";
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedIn = {firstName: 'Danish', lastName: 'Sharma'};
+
   return (
-    <main>
-        sidebar
-        {children}
+    <main className="flex h-screen w-full font-inter">
+        <Sidebar user={loggedIn} />
+        <div className="flex size-full flex-col">
+          <div className="root-layout">
+            <Image
+              src='/icons/logo.svg'
+              alt="logo"
+              width={30}
+              height={30}
+            />
+            <div>
+              <MobileNav user={loggedIn} />
+            </div>
+          </div>
+          {children}
+        </div>
     </main>
   );
 }
