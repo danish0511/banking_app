37d92a0729b78687fc27c96cc564a0cec4b624e3
danish0511 <dsdd0511@gmail.com>
2024-08-11 18:28:40 +0530

    auth pages

---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,17 @@
+'use server'
+
+export const signIn = async () => {
+    try {
+        // Mutation/database/make fetch
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+export const signUp = async (userData: SignUpParams) => {
+    try {
+        // Mutation/database/make fetch
+    } catch (error) {
+        console.error(error)
+    }
+}
