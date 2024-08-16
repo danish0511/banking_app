f67028aabb86d33ca5413b68238a504fad7710ef
danish0511 <dsdd0511@gmail.com>
2024-08-17 00:43:18 +0530

    auth final

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,15 +1,20 @@
 import MobileNav from "@/components/MobileNav";
 import Sidebar from "@/components/Sidebar";
+import { getLoggedInUser } from "@/lib/actions/user.actions";
 import type { Metadata } from "next";
 import { Inter, IBM_Plex_Serif } from "next/font/google";
 import Image from "next/image";
+import { redirect } from "next/navigation";
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const loggedIn = {firstName: 'Danish', lastName: 'Sharma'};
+
+  const loggedIn = await getLoggedInUser();
+
+  if (!loggedIn) redirect('/sign-in');
 
   return (
     <main className="flex h-screen w-full font-inter">
