e3e61bd53097e4928183ffa366707696961a402b
danish0511 <dsdd0511@gmail.com>
2024-08-16 23:34:33 +0530

    appwrite signup

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,17 +1,52 @@
-'use server'
+"use server";
+
+import { ID } from "node-appwrite";
+import { createAdminClient, createSessionClient } from "../appwrite";
+import { cookies } from "next/headers";
+import { parseStringify } from "../utils";
 
 export const signIn = async () => {
-    try {
-        // Mutation/database/make fetch
-    } catch (error) {
-        console.error(error)
-    }
-}
+  try {
+    // Mutation/database/make fetch
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 export const signUp = async (userData: SignUpParams) => {
-    try {
-        // Mutation/database/make fetch
-    } catch (error) {
-        console.error(error)
-    }
+    const {email, password, firstName, lastName} = userData;
+
+  try {
+    const { account } = await createAdminClient();
+
+    const newUserAccount = await account.create(
+        ID.unique(),
+        email,
+        password,
+        `${firstName} ${lastName}`
+    );
+    const session = await account.createEmailPasswordSession(email, password);
+
+    cookies().set("appwrite-session", session.secret, {
+      path: "/",
+      httpOnly: true,
+      sameSite: "strict",
+      secure: true,
+    });
+
+    return parseStringify(newUserAccount);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export async function getLoggedInUser() {
+  try {
+    const { account } = await createSessionClient();
+    const user = await account.get();
+
+    return parseStringify(user);
+  } catch (error) {
+    return null;
+  }
 }
